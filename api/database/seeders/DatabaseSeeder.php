ee882aa4fa481f2e982e2d32fa0b8d1787609f9b
Diego O. Mac <diego.a.olmedo@gmail.com>
2025-01-21 21:48:14 -0300

    Agrega seeder User

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -4,12 +4,18 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@diegodev.work';
+        $user->password = Hash::make('123456');
+        $user->save();
          Post::factory(100)->create();
     }
 }
